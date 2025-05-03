5489a3b7171c7d7d7dc28bb1b8b2c91f8b2a41b1
GitHub Action <action@github.com>
2025-05-03 01:09:20 +0000

    Bump version to 0.1.6

--- src/quantum_hash/__init__.py
+++ src/quantum_hash/__init__.py
@@ -5,7 +5,7 @@ This package provides quantum-inspired hash functions and key generation
 algorithms designed to be resistant to quantum computing attacks.
 """
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __author__ = 'Quantum Hash Team'
 __email__ = 'example@example.com'
 __license__ = 'MIT'
